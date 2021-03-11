2c57f7cab326efb6707df944bc168be9eef6add3
srikrishna <srikrishna3118@gmail.com>
2021-03-11 16:21:42 +0530

    testing using simple blob detector from opencv

--- Simulation/drone_port_sensor.py
+++ Simulation/drone_port_sensor.py
@@ -5,7 +5,7 @@ from __future__ import print_function
 import roslib
 import sys
 import rospy
-import cv2
+from cv2 import cv2
 import numpy as np
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
@@ -19,6 +19,17 @@ class image_parse:
         self.image_sub = rospy.Subscriber("/Marker0/c920/image_raw", Image,
                                           self.callback)
         self.set_s = 0
+
+        #self.params = cv2.SimpleBlobDetector_Params()
+
+        # Change thresholds
+        #self.params.minThreshold = 1
+
+        #self.params.filterByArea = True
+
+        #self.params.minArea = 1
+
+        #self.params.minDistBetweenBlobs = 5
 
     def callback(self, data):
         try:
@@ -36,14 +47,22 @@ class image_parse:
             self.prev_cv_image = cv_image
             self.set_s = 1
             print("Here Once")
+        #detector = cv2.SimpleBlobDetector_create(self.params)
 
-        #while 1:
         curr_gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
         prev_gray = cv2.cvtColor(self.prev_cv_image, cv2.COLOR_BGR2GRAY)
 
         frame_diff = cv2.absdiff(curr_gray, prev_gray)
+
+        #keypoints = detector.detect(frame_diff)
+
+        #print(keypoints)
+        #im_with_keypoints = cv2.drawKeypoints(frame_diff, keypoints, np.array([]), (0, 0, 255),
+        #                                      cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
         cv2.imshow("Image window", cv_image)
         cv2.imshow('frame diff ', frame_diff)
+        cv2.imshow("Keypoints", im_with_keypoints)
         cv2.waitKey(1)
         self.prev_cv_image = cv_image.copy()
 
